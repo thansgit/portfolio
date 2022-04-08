7ac41cae300a0083de3639e1ede939c8670c0c7c
Timo Hanski <timo.hanski@gmail.com>
2022-04-08 15:10:55 +0300

    Particle animation

--- src/App.js
+++ src/App.js
@@ -1,11 +1,71 @@
 
 import './App.css';
 import Navigation from './screens/navigationBar/NavigationBar'
+import Particles from 'react-tsparticles';
+
 
 function App() {
   return (
     <div className="App">
       <Navigation />
+      <Particles 
+        id="tsparticles"
+        options={{
+          background: {
+            color: {
+              value: "#212529",
+            },
+          },
+          fpsLimit: 60,
+          interactivity: {
+            events: {
+              onClick: {
+                enable: true,
+                mode: "push",
+              },
+              // onHover: {
+              //   enable: true,
+              //   mode: "repulse",
+              // },
+              resize: true,
+            },
+            modes: {
+              bubble: {
+                distance: 200,
+                duration: 2,
+                opacity: 0.8,
+                size: 40,
+              },
+              push: {
+                quantity: 4,
+              },
+              repulse: {
+                distance: 200,
+                duration: 0.4,
+              },
+            },
+          },
+          particles: {
+            color: {
+              value: "#ffffff",
+            },
+            links: {
+              color: "#ffff5f",
+              distance: 150,
+              enable: true,
+              opacity: 0.5,
+            },
+            move: {
+              direction: "none",
+              enable: true,
+              outMode: "bounce",
+              random: false,
+              speed: 1,
+              straight: false,
+            },
+          },
+        }}
+      />
     </div>
   );
 }
--- src/screens/navigationBar/NavigationBar.js
+++ src/screens/navigationBar/NavigationBar.js
@@ -4,25 +4,27 @@ import Navbar from 'react-bootstrap/Navbar'
 import Nav from 'react-bootstrap/Nav'
 import { navigationLinks } from '../../helpers/navigationLinks'
 
-
-function createLinks(){
+function createLinks() {
     return navigationLinks.map((e, idx) => (
         <Nav.Link key={idx} href={e.ref}>{e.name}</Nav.Link>
-    ) )
+    ))
 }
 
 function NavigationBar() {
     return (
         <div id="home">
-            <Navbar className="navigation__container" expand="md">
-                <Navbar.Brand style={{marginLeft: '1rem'}} href="#home">Timo Hanski</Navbar.Brand>
+            <Navbar className="navigation__container" style={{
+                zIndex: '2', position: 'fixed', top: '0', width: '100%'
+            }} collapseOnSelect
+                expand="md">
+                <Navbar.Brand style={{ marginLeft: '1rem' }} href="#home">Timo Hanski</Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse style={{ justifyContent: 'flex-end', marginRight: '1rem', borderColor: 'none' }}>
-                    <Nav className="links" style={{margin: '0 1rem' }}>
-                    {createLinks()}
+                    <Nav className="links" style={{ margin: '0 1rem' }}>
+                        {createLinks()}
                     </Nav>
                 </Navbar.Collapse>
-            </ Navbar>
+            </Navbar>
         </div>
     )
 }
