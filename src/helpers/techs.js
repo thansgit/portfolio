24fc810f052d33d488a471d01f5f62291906d2e4
Timo Hanski <timo.hanski@gmail.com>
2022-04-09 09:55:17 +0300

    added technologies

--- src/helpers/techs.js
+++ src/helpers/techs.js
@@ -1,6 +1,28 @@
+//List of icons
+//http://vorillaz.github.io/devicons/#/cheat
 export const techs = [
     {
-        name:'',
-        iconName:''
-    }
+        name:'React',
+        iconName:'react'
+    },
+    {
+        name:'Node',
+        iconName:'nodejs'
+    },
+    {
+        name:'Javascript',
+        iconName:'javascript'
+    },
+    {
+        name:'CSS3',
+        iconName:'css3'
+    },
+    {
+        name:'HTML 5',
+        iconName:'html5'
+    },
+    {
+        name:'Github',
+        iconName:'github'
+    },
 ]
--- src/screens/technologies/Technologies.jsx
+++ src/screens/technologies/Technologies.jsx
@@ -1,10 +1,25 @@
 import React from 'react'
+import './technologies.css'
 import DevIcon from 'devicon-react-svg';
+import {techs} from '../../helpers/techs'
 
 function Technologies() {
   return (
-    <div>
-      <DevIcon icon='react' />
+    <div className="technologies__container" id="technologies">
+        <div>
+            <h1 style={{textAlign:"center"}}>Some of the technologies that I have used</h1>
+        </div>
+        <div className="tech__cards__container">
+            {techs.map((e,idx) => {
+                return (
+                    <div key={idx} className="tech__card">
+                        <DevIcon className="tech__icons" icon={e.iconName} />
+                        <span style={{margin: '5px', fontSize:'1.5rem'}}>{e.name}</span>
+                    </div>
+                )
+            })}
+
+        </div>
     </div>
   )
 }
