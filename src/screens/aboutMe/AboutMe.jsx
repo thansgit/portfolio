890532586dae73c182815179f43b2e834256a20c
Timo Hanski <timo.hanski@gmail.com>
2022-04-21 18:06:40 +0300

    about

--- src/screens/aboutMe/AboutMe.jsx
+++ src/screens/aboutMe/AboutMe.jsx
@@ -11,7 +11,7 @@ function AboutMe() {
       <div className="about__text">
           <h1>ABOUT ME</h1>
           <p>
-            Hi! I'm Timo, a software developer and an IT-student from Tampere University. I am actively expanding my skills on the field and am currently focusing on web-development. At the moment I am more familiar with front end -development. I am looking to turn my interest in to a career. 
+            Hi! I'm Timo, a software developer and an IT-student from Tampere University. I am actively expanding my skills on the field and am currently focusing on web-development. At the moment I am more familiar with front end. I am looking to turn my interest in to a career. 
           </p>
       </div>
     </div>
